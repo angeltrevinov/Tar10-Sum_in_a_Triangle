9af9c0857d5c2c44abc0a886854110089bf294b2
Angel Odiel Trevino Villanueva <angel.trevinov@hotmail.com>
2018-03-08 14:34:58 -0600

    adding main

---
+++ main.cpp
@@ -0,0 +1,16 @@
+/*
+  Angel Odiel Treviño Villanueva
+  A01336559
+  Tarea: 10 Fecha: 8/Marzo Materia: Analisis y Diseño de Algoritmos
+*/
+
+#include <iostream>
+
+using namespace std;
+
+int main(){
+
+  /* Code Here */
+
+  return 0;
+}
