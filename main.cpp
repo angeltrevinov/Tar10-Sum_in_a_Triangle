bd43e7bcb0a0a4e563ecb9a1b77780bae8518938
Angel Odiel Trevino Villanueva <angel.trevinov@hotmail.com>
2018-03-09 15:21:32 -0600

    finishing problem

--- main.cpp
+++ main.cpp
@@ -9,18 +9,39 @@
 
 using namespace std;
 
+int MaxPath(int Triangulo[20][20], int N){ // to find the maximum posible path
+  for(int i = N - 2, cont = 0; i >= 0; i--, cont++){ // we are going from bottom to top
+    for(int j = 0; j < N - cont; j++){
+        if(Triangulo[i+1][j] < Triangulo[i+1][j+1]){
+          Triangulo[i][j] = Triangulo[i][j] + Triangulo[i+1][j+1];
+        }else{
+          Triangulo[i][j] = Triangulo[i][j] + Triangulo[i+1][j];
+        }
+    }
+  }
+  return Triangulo[0][0];
+}
+
 int main(){
 
-  int Triangulo[12][12]; // to store the triangle t
+  int Triangulo[20][20]; // to store the triangle that the user gives
   int Cases; // to store the number of cases;
   cin >> Cases;
   int N; //size of the matrix
+  vector <int> Answers; //to store answers
   while(Cases > 0){ // do these cases
+    cin >> N;
     for(int i = 0; i < N; i++){   //to go from top to bottom
-      for(int j = 0; j < N; j++){ //to go from left to right
-
+      for(int j = 0; j < i+1; j++){ //to go from left to right
+        cin >> Triangulo[i][j];
       }
     }
+    Answers.push_back(MaxPath(Triangulo, N));
+    Cases--;
+  }
+
+  for(int i = 0; i < Answers.size(); i++){
+    cout << Answers[i] << endl;
   }
 
   return 0;
