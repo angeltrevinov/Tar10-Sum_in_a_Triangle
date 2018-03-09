a124a1df77130a1519164df47c8b6231e2687aed
Angel Odiel Trevino Villanueva <angel.trevinov@hotmail.com>
2018-03-08 21:54:09 -0600

    starting

--- main.cpp
+++ main.cpp
@@ -5,12 +5,23 @@
 */
 
 #include <iostream>
+#include <vector>
 
 using namespace std;
 
 int main(){
 
-  /* Code Here */
+  int Triangulo[12][12]; // to store the triangle t
+  int Cases; // to store the number of cases;
+  cin >> Cases;
+  int N; //size of the matrix
+  while(Cases > 0){ // do these cases
+    for(int i = 0; i < N; i++){   //to go from top to bottom
+      for(int j = 0; j < N; j++){ //to go from left to right
+
+      }
+    }
+  }
 
   return 0;
 }
